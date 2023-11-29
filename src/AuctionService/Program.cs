02d163e1dcd0e7af1161746fc80c92dad1e93328
zakariabijoy <zakaria.bijoy@live.com>
2023-11-29 22:57:39 +0600

    Creating the first microservice: Shaping the data to return

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -14,6 +14,8 @@ builder.Services.AddDbContext<AuctionDbContext>(opt =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
