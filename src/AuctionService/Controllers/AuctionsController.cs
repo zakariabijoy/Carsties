d916f91c423cfac48dfe1d5e58a6906b6d7a3442
zakariabijoy <zakaria.bijoy@live.com>
2023-12-01 00:30:06 +0600

    Creating the first microservice: Adding the delete auction endpoint

--- src/AuctionService/Controllers/AuctionsController.cs
+++ src/AuctionService/Controllers/AuctionsController.cs
@@ -74,4 +74,23 @@ public class AuctionsController : ControllerBase
 
         return BadRequest("Problem saving changes");
     }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteAuction(Guid id)
+    {
+        var auction = await _context.Auctions.FindAsync(id);
+
+        if(auction == null) return  NotFound();
+
+        // TOD0: checkseller == username
+
+        _context.Auctions.Remove(auction);
+
+        var result = await _context.SaveChangesAsync() > 0;
+
+        if(result) return Ok();
+
+        return BadRequest("Could not update DB");
+    }
+
 }
