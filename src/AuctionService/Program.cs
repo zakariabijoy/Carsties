46f40ddafeb8770127f3ce7ecdd1d7aa74e3ea27
zakariabijoy <zakaria.bijoy@live.com>
2023-12-04 01:24:50 +0600

    RabbitMQ and MassTransit: Adding a message outbox for masstransit

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -18,6 +18,13 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddMassTransit(x =>
 {
+    x.AddEntityFrameworkOutbox<AuctionDbContext>(o => 
+    {
+        o.QueryDelay = TimeSpan.FromSeconds(10);
+        o.UsePostgres();
+        o.UseBusOutbox();
+    });
+
     x.UsingRabbitMq((context, cfg) =>
     {
         cfg.ConfigureEndpoints(context);
