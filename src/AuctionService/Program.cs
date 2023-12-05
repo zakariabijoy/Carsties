45eeea1d868936d3b1b362066de0d57d3e1d641f
zakariabijoy <zakaria.bijoy@live.com>
2023-12-06 01:01:21 +0600

    RabbitMQ and MassTransit: Consuming fault queues

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,3 +1,4 @@
+using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,10 @@ builder.Services.AddMassTransit(x =>
         o.UsePostgres();
         o.UseBusOutbox();
     });
+
+    x. AddConsumersFromNamespaceContaining<AuctionCreatedFaultConsumer>();
+
+    x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction",false));
 
     x.UsingRabbitMq((context, cfg) =>
     {
