177ba03eded4a84d72e2ba4a34df0ad7216376c7
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-12-19 00:37:01 +0600

    Identity Server: Configuring auth on the resource server

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -1,6 +1,7 @@
 using AuctionService.Consumers;
 using AuctionService.Data;
 using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -36,6 +37,15 @@ builder.Services.AddMassTransit(x =>
     });
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt => 
+                {
+                    opt.Authority = builder.Configuration["IdentityServiceUrl"];
+                    opt.RequireHttpsMetadata = false;
+                    opt.TokenValidationParameters.ValidateAudience = false;
+                    opt.TokenValidationParameters.NameClaimType = "username";
+                });
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -44,6 +54,7 @@ app.UseSwagger();
 app.UseSwaggerUI();
 
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
