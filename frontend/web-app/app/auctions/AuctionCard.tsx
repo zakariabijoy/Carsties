139a5bd7bb3257f27dc6c67e9e450d951145feb5
Md Zakaria Masud <zakaria.bijoy@live.com>
2023-12-30 01:45:18 +0600

    adding the client side NextJS application : Styling the auction cards

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image"
 
 type Props ={
     auction: any
@@ -5,6 +6,23 @@ type Props ={
 
 export default function AuctionCard({auction} : Props) {
   return (
-    <div>{auction.make}</div>
+    <a href="#">
+        <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
+            <div>
+                <Image
+                    src={auction.imageUrl}
+                    alt="image"
+                    fill
+                    className="object-cover"
+                    sizes="(max-width:768) 100vw, (max-width:1200px) 50vw, 25vw"
+                    priority
+                />
+            </div>
+        </div>
+        <div className="flex justify-between items-center mt-4">
+                <h3 className="text-gray-700">{auction.make} {auction.model}</h3>
+                <p className="font-semibold text-sm">{auction.year}</p>
+        </div>
+    </a>
   )
 }
