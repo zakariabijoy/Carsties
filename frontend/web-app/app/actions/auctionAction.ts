9e453108fcdcc8c63aef4378140057ea572f5707
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-01-02 00:54:56 +0600

    adding the client side NextJS application :  Adding a set page size option

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -1,8 +1,8 @@
 'use server'
 import { Auction, PagedResult } from "@/types";
 
-export async function getData(pageNumber:number = 1 ): Promise<PagedResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search?pageSize=4&pageNumber=${pageNumber}`);
+export async function getData(pageNumber:number, pageSize: number): Promise<PagedResult<Auction>> {
+    const res = await fetch(`http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`);
 
     if(!res.ok) throw new Error('Failed to fetch data');
 
--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -4,24 +4,27 @@ import AppPagination from "../components/AppPagination";
 import { useEffect, useState } from "react";
 import { Auction } from "@/types";
 import { getData } from "../actions/auctionAction";
+import Filters from "./Filters";
 
 
 export default function Listing() {
     const [auctions, setAuctions] = useState<Auction[]>([]);
     const [pageCount, setPageCount] = useState(0);
     const [pageNumber, setPageNumber] = useState(1);
+    const [pageSize, setPageSize] = useState(4);
     
     useEffect(() => {
-        getData(pageNumber).then(data =>{
+        getData(pageNumber, pageSize).then(data =>{
             setAuctions(data.results);
             setPageCount(data.pageCount);
         })
-    },[pageNumber]);
+    },[pageNumber, pageSize]);
 
     if(auctions.length === 0) return (<div><h3>Loading...</h3></div>)
 
     return (
         <>
+            <Filters pageSize={pageSize} setPageSize={setPageSize}/>
             <div className="grid grid-cols-4 gap-6">
                 {auctions.map((auction:any) => (
                     <AuctionCard auction={auction} key={auction.id}/>
