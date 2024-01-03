8de1511d6dd72cb80443b7cecc3d30d364b0165e
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-01-04 01:57:50 +0600

    adding the client side NextJS application : Adding a component to display when zero results

--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import AuctionCard from "./AuctionCard";
 import AppPagination from "../components/AppPagination";
 import { useEffect, useState } from "react";
@@ -6,46 +6,55 @@ import { Auction, PagedResult } from "@/types";
 import { getData } from "../actions/auctionAction";
 import Filters from "./Filters";
 import { useParamsStore } from "@/hooks/useParamsStore";
-import qs from 'query-string';
-
+import qs from "query-string";
+import EmptyFilter from "../components/EmptyFilter";
 
 export default function Listing() {
-    const [data, setData] = useState<PagedResult<Auction>>();
-    const params = useParamsStore(state => ({
-        pageNumber: state.pageNumber,
-        pageSize: state.pageSize,
-        searchTerm: state.searchTerm,
-        orderBy: state.orderBy,
-        filterBy: state.filterBy
-    }));
-    
-    const setParams = useParamsStore(state => state.setParams);
-    const url = qs.stringifyUrl({url: '', query: params});
-    
-    function setPageNumber(pageNumber: number){
-        setParams({pageNumber});
-    }
+  const [data, setData] = useState<PagedResult<Auction>>();
+  const params = useParamsStore((state) => ({
+    pageNumber: state.pageNumber,
+    pageSize: state.pageSize,
+    searchTerm: state.searchTerm,
+    orderBy: state.orderBy,
+    filterBy: state.filterBy,
+  }));
 
-    useEffect(() => {
-        getData(url).then(data =>{
-            setData(data);
-        })
-    },[url]);
+  const setParams = useParamsStore((state) => state.setParams);
+  const url = qs.stringifyUrl({ url: "", query: params });
 
-    if(!data) return <p>Loading...</p>;
+  function setPageNumber(pageNumber: number) {
+    setParams({ pageNumber });
+  }
 
-    return (
+  useEffect(() => {
+    getData(url).then((data) => {
+      setData(data);
+    });
+  }, [url]);
+
+  if (!data) return <p>Loading...</p>;
+
+  return (
+    <>
+      <Filters />
+      {data.totalCount === 0 ? (
+        <EmptyFilter showReset />
+      ) : (
         <>
-            <Filters/>
-            <div className="grid grid-cols-4 gap-6">
-                {data.results.map((auction:any) => (
-                    <AuctionCard auction={auction} key={auction.id}/>
-                ))}      
-            </div>
-            <div className="flex justify-center mt-4">
-                <AppPagination currentPage={params.pageNumber} pageCount={data.pageCount} pageChanged={setPageNumber}/>
-            </div>
+          <div className="grid grid-cols-4 gap-6">
+            {data.results.map((auction: any) => (
+              <AuctionCard auction={auction} key={auction.id} />
+            ))}
+          </div>
+          <div className="flex justify-center mt-4">
+            <AppPagination
+              currentPage={params.pageNumber}
+              pageCount={data.pageCount}
+              pageChanged={setPageNumber}
+            />
+          </div>
         </>
-        
-    )
+      )}
+    </>
+  );
 }
