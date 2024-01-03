ca2456b9b48f5780888b3f37f7f998e646b5ff60
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-01-04 01:40:29 +0600

    adding the client side NextJS application : Adding the filtering functionality

--- frontend/web-app/app/auctions/Listing.tsx
+++ frontend/web-app/app/auctions/Listing.tsx
@@ -15,7 +15,8 @@ export default function Listing() {
         pageNumber: state.pageNumber,
         pageSize: state.pageSize,
         searchTerm: state.searchTerm,
-        orderBy: state.orderBy
+        orderBy: state.orderBy,
+        filterBy: state.filterBy
     }));
     
     const setParams = useParamsStore(state => state.setParams);
