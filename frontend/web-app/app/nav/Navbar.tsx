fbdab7dce346af2f09e0bcf3d154f3e7dc2f188b
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-01-04 00:32:50 +0600

    adding the client side NextJS application : Adding a search bar

--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,4 +1,5 @@
 import { AiOutlineCar } from "react-icons/ai"
+import Search from "./Search"
 
 export default function Navbar() {
   return (
@@ -7,7 +8,7 @@ export default function Navbar() {
         <AiOutlineCar size={34} />
         <div>Carsties Auctions</div>
       </div>
-      <div>Search</div>
+      <Search/>
       <div>Login</div>
     </header>
   )
