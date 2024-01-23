3cacef4399fd663b12baa870ebfde1781593cf09
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-01-24 01:41:09 +0600

    Client side identity with NextAuth (AuthJs): Getting the access token to use to authenticate to our resource server

--- frontend/web-app/app/session/page.tsx
+++ frontend/web-app/app/session/page.tsx
@@ -1,11 +1,12 @@
 
 import React from 'react'
 import Heading from '../components/Heading';
-import { getSession } from '../actions/authActions';
+import { getSession, getTokenWorkaround } from '../actions/authActions';
 import AuthTest from './AuthTest';
 
 export default async function Session() {
     const session = await getSession();
+    const token = await getTokenWorkaround();
 
   return (
     <div>
@@ -16,6 +17,10 @@ export default async function Session() {
         </div>
         <div className='mt-4'>
           <AuthTest/>
+        </div>
+        <div className='bg-green-200 border-2 border-blue-500 mt-4'>
+            <h3 className='text-lg'>Token data</h3>
+            <pre className='overflow-auto'>{JSON.stringify(token, null, 2)}</pre>
         </div>
     </div>
   )
