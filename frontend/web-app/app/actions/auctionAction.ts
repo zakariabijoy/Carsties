87243e95068b0ae707a5fc95e1b938d7b22a5b1f
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-15 00:40:48 +0600

    Crud in the client app: Adding the auction  details page content

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -22,3 +22,7 @@ export async function updateAuctionTest() {
 export async function createAuction(data: FieldValues) {
   return await fetchWrapper.post("auctions", data);
 }
+
+export async function getDetailedViewData(id: string): Promise<Auction> {
+  return await fetchWrapper.get(`auctions/${id}`);
+}
