2c81cffdd17556f4fd9a6e11431efa336bcafb63
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-15 00:21:02 +0600

    Crud in the client app: Adding react hot toast to display notifications if something goes wrong

--- frontend/web-app/app/auctions/AuctionForm.tsx
+++ frontend/web-app/app/auctions/AuctionForm.tsx
@@ -7,6 +7,7 @@ import Input from "../components/Input";
 import DateInput from "../components/DateInput";
 import { createAuction } from "../actions/auctionAction";
 import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
 
 export default function AuctionForm() {
   const router = useRouter();
@@ -28,11 +29,11 @@ export default function AuctionForm() {
     try {
       const res = await createAuction(data);
       if (res.error) {
-        throw new Error(res.error);
+        throw res.error;
       }
       router.push(`details/${res.id}`);
-    } catch (error) {
-      console.log(error);
+    } catch (error: any) {
+      toast.error(error.status + " " + error.message);
     }
   }
 
