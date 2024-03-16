e539a0f71e3ecd7a37b0672fb8d7aef410bf6139
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-17 03:07:50 +0600

    Creating the bid service: Adding the DTOs and Automapper

--- src/BiddingService/Program.cs
+++ src/BiddingService/Program.cs
@@ -39,6 +39,8 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                     opt.TokenValidationParameters.NameClaimType = "username";
                 });
 
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
