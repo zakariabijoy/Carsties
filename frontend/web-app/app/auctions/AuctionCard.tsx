15dd46f331c8e53a680212f3f2513f0c1f094e69
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-21 01:41:44 +0600

    Adding the bids/notifications to the client: Updating the Auction cards with the current high price

--- frontend/web-app/app/auctions/AuctionCard.tsx
+++ frontend/web-app/app/auctions/AuctionCard.tsx
@@ -1,27 +1,36 @@
-import { Auction } from "@/types"
-import CarImage from "./CarImage"
-import CountdownTimer from "./CountdownTimer"
-import Link from "next/link"
+import { Auction } from "@/types";
+import CarImage from "./CarImage";
+import CountdownTimer from "./CountdownTimer";
+import Link from "next/link";
+import CurrentBid from "./CurrentBid";
 
-type Props ={
-    auction: Auction
-}
+type Props = {
+  auction: Auction;
+};
 
-export default function AuctionCard({auction} : Props) {
+export default function AuctionCard({ auction }: Props) {
   return (
     <Link href={`/auctions/details/${auction.id}`} className="group">
-        <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
-            <div>
-                <CarImage imageUrl={auction.imageUrl}/>
-                <div className="absolute bottom-0 left-2">
-                    <CountdownTimer auctionEnd={auction.auctionEnd}/>
-                </div>
-            </div>
+      <div className="w-full bg-gray-200 aspect-w-16 aspect-h-10 rounded-lg overflow-hidden">
+        <div>
+          <CarImage imageUrl={auction.imageUrl} />
+          <div className="absolute bottom-2 left-2">
+            <CountdownTimer auctionEnd={auction.auctionEnd} />
+          </div>
+          <div className="absolute top-2 right-2">
+            <CurrentBid
+              reservePrice={auction.reservePrice}
+              amount={auction.currentHighBid}
+            />
+          </div>
         </div>
-        <div className="flex justify-between items-center mt-4">
-                <h3 className="text-gray-700">{auction.make} {auction.model}</h3>
-                <p className="font-semibold text-sm">{auction.year}</p>
-        </div>
+      </div>
+      <div className="flex justify-between items-center mt-4">
+        <h3 className="text-gray-700">
+          {auction.make} {auction.model}
+        </h3>
+        <p className="font-semibold text-sm">{auction.year}</p>
+      </div>
     </Link>
-  )
+  );
 }
