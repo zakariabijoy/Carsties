7e3b9f4c4d0bccabc39c14cf7505e2246b522027
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-25 01:55:52 +0600

    Adding the bids/notifications to the client: Creating a bid form & bug fix

---
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import { placeBidForAuction } from "@/app/actions/auctionAction";
+import { numberWithCommas } from "@/app/lib/numberWithComma";
+import { useBidStore } from "@/hooks/useBidStore";
+import React from "react";
+import { FieldValues, useForm } from "react-hook-form";
+
+type Props = {
+  auctionId: string;
+  highBid: number;
+};
+export default function BidForm({ auctionId, highBid }: Props) {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const addBid = useBidStore((state) => state.addBid);
+
+  function onSubmit(data: FieldValues) {
+    placeBidForAuction(auctionId, +data.amount).then((bid) => {
+      addBid(bid);
+      reset();
+    });
+  }
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="flex items-center border-2 rounded-lg py-2"
+    >
+      <input
+        type="number"
+        {...register("amount")}
+        className="input-custom text-sm text-gray-600"
+        placeholder={`Enter your bid (minimum bid is $${numberWithCommas(
+          highBid + 1
+        )})`}
+      />
+    </form>
+  );
+}
--- frontend/web-app/app/nav/Navbar.tsx
+++ frontend/web-app/app/nav/Navbar.tsx
@@ -1,22 +1,17 @@
-import Search from "./Search"
-import Logo from "./Logo"
-import LoginButton from "./LoginButton"
-import { getCurrentuser } from "../actions/authActions"
+import Search from "./Search";
+import Logo from "./Logo";
+import LoginButton from "./LoginButton";
+import { getCurrentuser } from "../actions/authActions";
 import UserActions from "./UserActions";
 
 export default async function Navbar() {
   const user = await getCurrentuser();
 
   return (
-    <header className="sticky top-0 z=50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
-      <Logo/>
-      <Search/>
-      {user ? (
-        <UserActions user={user}/>
-      ) : (
-        <LoginButton/>
-      )}
-      
+    <header className="sticky top-0 z-50 flex justify-between bg-white p-5 items-center text-gray-800 shadow-md">
+      <Logo />
+      <Search />
+      {user ? <UserActions user={user} /> : <LoginButton />}
     </header>
-  )
+  );
 }
