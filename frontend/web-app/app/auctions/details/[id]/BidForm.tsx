4104a9d504796c194e5fab2fbb12b0ed914f0cc7
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-25 02:15:47 +0600

    Adding the bids/notifications to the client: Updating the error handling

--- frontend/web-app/app/auctions/details/[id]/BidForm.tsx
+++ frontend/web-app/app/auctions/details/[id]/BidForm.tsx
@@ -5,6 +5,7 @@ import { numberWithCommas } from "@/app/lib/numberWithComma";
 import { useBidStore } from "@/hooks/useBidStore";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
 
 type Props = {
   auctionId: string;
@@ -20,10 +21,15 @@ export default function BidForm({ auctionId, highBid }: Props) {
   const addBid = useBidStore((state) => state.addBid);
 
   function onSubmit(data: FieldValues) {
-    placeBidForAuction(auctionId, +data.amount).then((bid) => {
-      addBid(bid);
-      reset();
-    });
+    placeBidForAuction(auctionId, +data.amount)
+      .then((bid) => {
+        if (bid.error) throw bid.error;
+        addBid(bid);
+        reset();
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      });
   }
 
   return (
