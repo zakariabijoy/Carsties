2ee7fb977c6e83a5f72b994cecbaf5628d6c2a1b
Md Zakaria Masud <zakaria.bijoy@live.com>
2024-03-26 03:58:22 +0600

    Adding the bids/notifications to the client: Adding SignalR to the client app

---
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useAuctionStore } from "@/hooks/useAuctionStore";
+import { useBidStore } from "@/hooks/useBidStore";
+import { Bid } from "@/types";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
+import React, { ReactNode, useEffect, useState } from "react";
+
+type Props = {
+  children: ReactNode;
+};
+export default function SignalRProvider({ children }: Props) {
+  const [connection, setConnection] = useState<HubConnection | null>(null);
+  const setCurrentPrice = useAuctionStore((state) => state.setCurrentPrice);
+  const addBid = useBidStore((state) => state.addBid);
+
+  useEffect(() => {
+    const newConnection = new HubConnectionBuilder()
+      .withUrl("http://localhost:6001/notifications")
+      .withAutomaticReconnect()
+      .build();
+
+    setConnection(newConnection);
+  }, []);
+
+  useEffect(() => {
+    if (connection) {
+      connection
+        .start()
+        .then(() => {
+          console.log("Connected to notofication hub");
+
+          connection.on("BidPlaced", (bid: Bid) => {
+            console.log("Bid placed event received");
+            if (bid.bidStatus.includes("Accepted")) {
+              setCurrentPrice(bid.auctionId, bid.amount);
+            }
+          });
+        })
+        .catch((err) => console.log(err));
+    }
+
+    return () => {
+      connection?.stop;
+    };
+  }, [connection, setCurrentPrice]);
+
+  return children;
+}
