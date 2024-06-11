d53228cd06ee4484ef51c88df21ebd58c970bc4d
Zakaria Masud <zakaria.masud@surbanajurong.com>
2024-06-12 03:17:59 +0600

    Testing: Mocking things

---
+++ test/AuctionService.UnitTests/AuctionControllerTests.cs
@@ -0,0 +1,32 @@
+﻿using AuctionService.Controllers;
+using AuctionService.Data.Repositories;
+using AuctionService.RequestHelpers;
+using AutoFixture;
+using AutoMapper;
+using MassTransit;
+using Moq;
+
+namespace AuctionService.UnitTests;
+
+public class AuctionControllerTests
+{
+    private readonly Mock<IAuctionRepository> _auctionRepo;
+    private readonly Mock<IPublishEndpoint> _publishEndpoint;
+    private readonly Fixture _fixture;
+    private readonly AuctionsController _controller;
+    private readonly IMapper _mapper;
+
+    public AuctionControllerTests()
+    {
+        _auctionRepo = new Mock<IAuctionRepository>();
+        _publishEndpoint = new Mock<IPublishEndpoint>();
+        _fixture = new Fixture();
+
+        var mockMapper = new MapperConfiguration(mc => {
+            mc.AddMaps(typeof(MappingProfiles).Assembly);
+        }).CreateMapper().ConfigurationProvider;
+
+        _mapper = new Mapper(mockMapper);
+        _controller = new AuctionsController(_auctionRepo.Object, _mapper, _publishEndpoint.Object);
+    }
+}
