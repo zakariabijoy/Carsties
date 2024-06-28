986c79226a7fcb5799a545bcf66e07d98532326b
zakariabijoy <zakaria.bijoy@live.com>
2024-06-29 03:43:11 +0600

    Testing: Creating an integration test for the GET method part 1

---
+++ test/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Net.Http.Json;
+using AuctionService.Data;
+using AuctionService.DTOs;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Writers;
+
+namespace AuctionService.IntegrationTests;
+
+public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsyncLifetime
+{
+    private readonly CustomWebAppFactory _factory;
+    private readonly HttpClient _httpClient;
+
+    public AuctionControllerTests(CustomWebAppFactory factory)
+    {
+        _factory = factory;
+        _httpClient = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task GetAuctions_ShouldReturn3Auctions()
+    {
+        //arrange
+
+        //act
+        var response = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("api/auctions");
+
+        // assert
+        Assert.Equal(3, response.Count);
+    }
+
+    public Task InitializeAsync() => Task.CompletedTask;
+    public Task DisposeAsync()
+    {
+        using var scope = _factory.Services.CreateScope();
+        var db =  scope.ServiceProvider.GetService<AuctionDbContext>();
+        DbHelper.ReInitDbForTest(db);
+        return Task.CompletedTask;  
+    }
+}
