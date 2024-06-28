012ac58aa77b62810bef4d0d7318aef71f01e273
zakariabijoy <zakaria.bijoy@live.com>
2024-06-29 03:55:36 +0600

    Testing: Creating an integration test for the GET method part 2

--- test/AuctionService.IntegrationTests/AuctionControllerTests.cs
+++ test/AuctionService.IntegrationTests/AuctionControllerTests.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.Net;
 using System.Net.Http.Json;
 using AuctionService.Data;
 using AuctionService.DTOs;
@@ -11,6 +12,7 @@ public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsync
 {
     private readonly CustomWebAppFactory _factory;
     private readonly HttpClient _httpClient;
+    private const string GT_Id = "afbee524-5972-4075-8800-7d1f9d7b0a0c";
 
     public AuctionControllerTests(CustomWebAppFactory factory)
     {
@@ -21,13 +23,49 @@ public class AuctionControllerTests : IClassFixture<CustomWebAppFactory>, IAsync
     [Fact]
     public async Task GetAuctions_ShouldReturn3Auctions()
     {
-        //arrange
+        //arrange?
 
         //act
         var response = await _httpClient.GetFromJsonAsync<List<AuctionDto>>("api/auctions");
 
         // assert
         Assert.Equal(3, response.Count);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithValidId_ShouldReturnAuction()
+    {
+        //arrange?
+
+        //act
+        var response = await _httpClient.GetFromJsonAsync<AuctionDto>($"api/auctions/{GT_Id}");
+
+        // assert
+        Assert.Equal("GT", response.Model);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInValidId_ShouldReturn404()
+    {
+        //arrange?
+
+        //act
+        var response = await _httpClient.GetAsync($"api/auctions/{Guid.NewGuid()}");
+
+        // assert
+        Assert.Equal(HttpStatusCode.NotFound, response.StatusCode);
+    }
+
+    [Fact]
+    public async Task GetAuctionById_WithInValidGuid_ShouldReturn400()
+    {
+        //arrange?
+
+        //act
+        var response = await _httpClient.GetAsync($"api/auctions/notaguid");
+
+        // assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
     public Task InitializeAsync() => Task.CompletedTask;
